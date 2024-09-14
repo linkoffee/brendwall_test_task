9bbfacaa62fa966ebc29c1f81e25458b87378528
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-09-14 22:28:07 +0500

    create product model

---
+++ backend/product/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from .constants import (
+    CHAR_OUTPUT_LIMIT,
+    MAX_PRODUCT_NAME_LEN,
+    PRICE_DECIMAL_PLACES,
+    PRICE_MAX_DIGITS,
+)
+
+
+class Product(models.Model):
+    """Модель продукта."""
+
+    name = models.CharField(
+        unique=True,
+        max_length=MAX_PRODUCT_NAME_LEN
+    )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=PRICE_MAX_DIGITS,
+        decimal_places=PRICE_DECIMAL_PLACES
+    )
+
+    def __str__(self):
+        return self.name[:CHAR_OUTPUT_LIMIT]
