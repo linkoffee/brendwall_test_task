65e45be95861c0e2080bf26a3050841c23be36b2
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-09-15 18:33:22 +0500

    register product model in django admin

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+admin.site.empty_value_display = 'пусто'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админ-модель продукта."""
+
+    list_display = ('name', 'description', 'price')
+    search_fields = ('name',)
+    list_filter = ('name', 'price')
